d5c1aa44ad2eedf2838a7493227f9b03565799b0
IcyDiamond <117333057+IcyDiamond@users.noreply.github.com>
2023-09-03 22:42:17 -0700

    fixed update file

--- update.py
+++ update.py
@@ -5,6 +5,7 @@ import zipfile
 import os
 import shutil
 import io
+import subprocess
 
 
 # Define your GitHub repository URL and the version tracking file URL
@@ -15,15 +16,18 @@ repository_url = f'https://github.com/{authors}/PythonDesktop-Files'
 version_file_url = f'https://raw.githubusercontent.com/{authors}/PythonDesktop-Files/{repo}/PythonDesktop.py'
 
 # Get the current version of the script
-with open('PythonDesktop.py', 'r') as f:
-    current_version = f.read().split("\n")
-    for line in current_version:
-        if "update_version =" in line:
-            value = line.split("update_version = ")[1]
-            value = value.replace("\r","")
-            ver1 = float(value)
-            current_version = ver1
-            break
+try:
+    with open('PythonDesktop.py', 'r') as f:
+        current_version = f.read().split("\n")
+        for line in current_version:
+            if "update_version =" in line:
+                value = line.split("update_version = ")[1]
+                value = value.replace("\r","")
+                ver1 = float(value)
+                current_version = ver1
+                break
+except FileNotFoundError:
+    current_version='Null'
 
 # Check the latest version on GitHub
 response = requests.get(version_file_url)
@@ -68,17 +72,16 @@ if response.status_code == 200:
                         else:
                             print(f'Failed to download files. Status code: {response.status_code}')
 
-                        # Define the content of the new Python script
-                        new_script_content = """
+                print('Update complete. Please restart the script.')
+        # Define the content of the new Python script
+        new_script_content = """
 import os
 import shutil
-try:
-    for i in os.listdir('PythonDesktop-Files-Main'):
-        shutil.move(f"PythonDesktop-Files-Main/{i}", os.getcwd())
-    shutil.rmtree('PythonDesktop-Files-Main')
-    shutil.rmtree('update.py')
-except:
-    pass
+os.remove('update.py')
+for i in os.listdir('PythonDesktop-Files-Main'):
+    shutil.move(f"PythonDesktop-Files-Main/{i}", os.getcwd())
+    print(f'moving {i}')
+shutil.rmtree('PythonDesktop-Files-Main')
 
 def delete_self():
     try:
@@ -103,21 +106,22 @@ if __name__ == "__main__":
     delete_self()
 """
 
-                        # Specify the file name for the new script
-                        new_script_file = "temp_move_files.py"
+        # Specify the file name for the new script
+        new_script_file = "temp_move_files.py"
 
-                        # Write the content to the new script file
-                        with open(new_script_file, "w") as file:
-                            file.write(new_script_content)
+        # Write the content to the new script file
+        print('writing file to move Files')
+        with open(new_script_file, "w") as file:
+            file.write(new_script_content)
 
-                        # Import the necessary module to run the new script
-                        import subprocess
+        # Import the necessary module to run the new script
 
-                        # Run the new script using the Python interpreter
-                        subprocess.Popen(["python", new_script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
-                print('Update complete. Please restart the script.')
+        # Run the new script using the Python interpreter
+        print('Moving Files')
+        subprocess.Popen(["python", new_script_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     else:
         print('You are already using the latest version.')
 else:
     print('Failed to check for updates.')
+
+    
